b6a2e30d9dd28f685838c47aa8a143f08c516a47
Fairfay <98214276+Fairfay@users.noreply.github.com>
2023-03-20 11:53:04 +0700

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+int a = 1;
+double per = ((Math.Pow(-1, a)) / (a * (2 * a + 1)));
+double summa = 0;
+Console.WriteLine("Введите E=");
+double E= double.Parse(Console.ReadLine());
+while (Math.Abs(per) >= E)
+{
+    summa += per;
+    a++;
+    per = ((Math.Pow(-1, a)) / (a * (2 * a + 1)));
+}
+Console.WriteLine("Сумма=" + summa);
+Console.WriteLine("Последнее слагаемое=" + per);
+Console.WriteLine("Последнее слагаемое по модулю=" + Math.Abs(per));
+Console.WriteLine("Кол-во слагаемых-=" + a);
